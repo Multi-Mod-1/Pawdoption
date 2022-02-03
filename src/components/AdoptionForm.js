54cd0fb25d6e9ad70a8cb3a62ca0e58d335fd510
CJGarciaMusic <CJGarciaMusic@gmail.com>
2022-02-03 15:13:00 -0500

    adoption form added

--- src/components/AdoptionForm.js
+++ src/components/AdoptionForm.js
@@ -1,45 +1,26 @@
-import * as React from "react";
+import "./AdoptionForm.css";
 
-import Box from "@mui/material/Box";
-import Switch from "@mui/material/Switch";
-import Paper from "@mui/material/Paper";
-import Slide from "@mui/material/Slide";
-import FormControlLabel from "@mui/material/FormControlLabel";
-
-const icon = (
-  <Paper sx={{ m: 1 }} elevation={4}>
-    {
-      <div>
-        <label>
-          <input type="checkbox" />
-          <div class="card">
-            <div class="front">Front</div>
-            <div class="back">Back</div>
-          </div>
-        </label>
-      </div>
-    }
-  </Paper>
-);
-
-export default function SimpleSlide() {
-  const [checked, setChecked] = React.useState(false);
-
-  const handleChange = () => {
-    setChecked((prev) => !prev);
-  };
-
+function AdoptionForm() {
   return (
-    <Box sx={{ height: 180 }}>
-      <Box sx={{ width: `calc(100px + 16px)` }}>
-        <FormControlLabel
-          control={<Switch checked={checked} onChange={handleChange} />}
-          label="Show"
-        />
-        <Slide direction="left" in={checked} mountOnEnter unmountOnExit>
-          {icon}
-        </Slide>
-      </Box>
-    </Box>
+      <div class="FormHeader">
+        <link href='https://fonts.googleapis.com/css?family=Gothic A1' rel='stylesheet'></link>
+        <div>
+          <img alt="cute dog" src="https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/30/pembroke-welsh-corgi.jpg" class="clip-circle"/> 
+          <h3>Cuty McCuterson</h3>
+        </div>
+        <div class="AdoptionBody">
+          <form class="myForm">
+            <label for="customer_name">Your Name </label>
+            <input type="text" name="customer_name" id="customer_name" required/>
+            <label for="email_address">Your Email </label>
+            <input type="email" name="email_address" id="email_address"/>
+            <label for="comments">Message</label>
+            <textarea name="comments" id="comments" maxlength="500" placeholder="Please write a message to the foster for why you want to adopt this dog"></textarea>
+          </form>
+        </div>
+        <button className="single-page-button">Back To Details</button>
+      </div>
   );
 }
+
+export default AdoptionForm;
